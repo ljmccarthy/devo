bd4c177044c7eaa3421a4614cf5f83847bc015ca
Luke McCarthy <luke@iogopro.co.uk>
2009-12-29 17:36:00 +0000

    Patterns were not matching full string properly, had to put ^ and $ in every pattern.

--- syntax.py
+++ syntax.py
@@ -10,9 +10,9 @@ syntax_dict = dict((syntax.ident, syntax) for syntax in syntax_list)
 def filename_syntax_re():
     patterns = []
     for syntax in syntax_list:
-        ptn = "|".join(re.escape(ext).replace("\\*", ".*") for ext in syntax.extensions)
+        ptn = "|".join("^%s$" % re.escape(ext).replace("\\*", ".*") for ext in syntax.extensions)
         ptn = "(?P<%s>%s)" % (syntax.ident, ptn)
         patterns.append(ptn)
-    return re.compile("^%s$" % "|".join(patterns))
+    return re.compile("%s" % "|".join(patterns))
 
 filename_syntax_re = filename_syntax_re()
