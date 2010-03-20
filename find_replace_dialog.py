ff062c7bf9cd6e789da3751f7a592d4ffa78f0db
Luke McCarthy <luke@iogopro.co.uk>
2010-03-20 07:12:53 +0000

    Compile find pattern regular expression when property is set. Renamed ptn_* to *.

--- find_replace_dialog.py
+++ find_replace_dialog.py
@@ -4,21 +4,24 @@ import dialogs
 from dialog_util import bind_escape_key
 
 class FindReplaceDetails(object):
-    def __init__(self, ptn_find, ptn_replace, case=False, reverse=False, regexp=False):
-        self.ptn_find = ptn_find
-        self.ptn_replace = ptn_replace
+    def __init__(self, find, replace, case=False, reverse=False, regexp=False):
         self.case = case
         self.reverse = reverse
         self.regexp = regexp
+        self.__find = None
+        self.find = find
+        self.replace = replace
 
-    def _GetFindPattern(self, editor):
-        ptn = self.ptn_find
-        if not self.regexp:
-            ptn = re.escape(ptn)
-        try:
-            return re.compile(ptn, 0 if self.case else re.IGNORECASE)
-        except re.error, e:
-            dialogs.error(editor, "Invalid regular expression:\n\n" + str(e).capitalize())
+    @property
+    def find(self):
+        return self.__find
+
+    @find.setter
+    def find(self, ptn):
+        self.rx_find = re.compile(
+            ptn if self.regexp else re.escape(ptn),
+            0 if self.case else re.IGNORECASE)
+        self.__find = ptn
 
     def _IterFindLines(self, editor, wrap=True):
         init_pos = editor.GetSelection()[1]
@@ -42,46 +45,40 @@ class FindReplaceDetails(object):
         text = editor.GetSelectedText()
         if text:
             if self.regexp:
-                ptn = self._GetFindPattern(editor)
                 try:
-                    repl = ptn.sub(self.ptn_replace, editor.GetSelectedText(), 1)
+                    repl = self.rx_find.sub(self.replace, editor.GetSelectedText(), 1)
                     editor.ReplaceSelection(repl)
                 except re.error, e:
                     dialogs.error(editor, "Replace error:\n\n" + str(e).capitalize())
                     return False
             else:
-                editor.ReplaceSelection(self.ptn_replace)
+                editor.ReplaceSelection(self.replace)
 
     def Find(self, editor, wrap=True):
-        ptn = self._GetFindPattern(editor)
-        if ptn:
-            for pos, line in self._IterFindLines(editor, wrap):
-                m = ptn.search(line)
-                if m and m.start() != m.end():
-                    editor.SetSelection(pos + m.start(), pos + m.end())
-                    return True
+        for pos, line in self._IterFindLines(editor, wrap):
+            m = self.rx_find.search(line)
+            if m and m.start() != m.end():
+                editor.SetSelection(pos + m.start(), pos + m.end())
+                return True
         return False
 
     def Replace(self, editor):
-        ptn = self._GetFindPattern(editor)
-        if ptn.match(editor.GetSelectedText()):
+        if self.rx_find.match(editor.GetSelectedText()):
             self._ReplaceSelected(editor)
         return self.Find(editor)
 
     def ReplaceAll(self, editor):
         count = 0
-        ptn = self._GetFindPattern(editor)
-        if ptn:
-            editor.SetSelection(0, 0)
-            m = True
-            while m:
-                for pos, line in self._IterFindLines(editor, wrap=False):
-                    m = ptn.search(line)
-                    if m and m.start() != m.end():
-                        editor.SetSelection(pos + m.start(), pos + m.end())
-                        self._ReplaceSelected(editor)
-                        count += 1
-                        break
+        editor.SetSelection(0, 0)
+        m = True
+        while m:
+            for pos, line in self._IterFindLines(editor, wrap=False):
+                m = self.rx_find.search(line)
+                if m and m.start() != m.end():
+                    editor.SetSelection(pos + m.start(), pos + m.end())
+                    self._ReplaceSelected(editor)
+                    count += 1
+                    break
         return count
 
 ID_GO_TO_START = wx.NewId()
@@ -134,8 +131,8 @@ class FindReplaceDialog(wx.Dialog):
         self.Fit()
 
         if details is not None:
-            self.text_find.SetValue(details.ptn_find)
-            self.text_replace.SetValue(details.ptn_replace)
+            self.text_find.SetValue(details.find)
+            self.text_replace.SetValue(details.replace)
             self.check_regexp.SetValue(details.regexp)
             self.check_reverse.SetValue(details.reverse)
 
@@ -152,31 +149,38 @@ class FindReplaceDialog(wx.Dialog):
         self.editor.SetSelection(0, 0)
 
     def OnFind(self, evt):
-        details = self.GetFindDetails()
-        if not details.Find(self.editor):
-            dialogs.info(self, "Pattern not found: '%s'" % details.ptn_find, "Find")
+        details = self.GetFindDetails(True)
+        if details and not details.Find(self.editor):
+            dialogs.info(self, "Pattern not found: '%s'" % details.find, "Find")
 
     def OnReplace(self, evt):
-        details = self.GetFindDetails()
-        if not details.Replace(self.editor):
-            dialogs.info(self, "Pattern not found: '%s'" % details.ptn_find, "Replace")
+        details = self.GetFindDetails(True)
+        if details and not details.Replace(self.editor):
+            dialogs.info(self, "Pattern not found: '%s'" % details.find, "Replace")
 
     def OnReplaceAll(self, evt):
-        details = self.GetFindDetails()
+        details = self.GetFindDetails(True)
+        if not details:
+            return
         count = details.ReplaceAll(self.editor)
         if count > 0:
             dialogs.info(self,
-                "Replaced %d instances of '%s'" % (count, details.ptn_find),
+                "Replaced %d instances of '%s'" % (count, details.find),
                 "Replace All")
         else:
             dialogs.info(self,
-                "Pattern not found: '%s'" % details.ptn_find,
+                "Pattern not found: '%s'" % details.find,
                 "Replace All")
 
-    def GetFindDetails(self):
-        return FindReplaceDetails(
-            ptn_find = self.text_find.GetValue(),
-            ptn_replace = self.text_replace.GetValue(),
-            case = self.check_case.GetValue(),
-            reverse = self.check_reverse.GetValue(),
-            regexp = self.check_regexp.GetValue())
+    def GetFindDetails(self, show_error=False):
+        try:
+            return FindReplaceDetails(
+                find = self.text_find.GetValue(),
+                replace = self.text_replace.GetValue(),
+                case = self.check_case.GetValue(),
+                reverse = self.check_reverse.GetValue(),
+                regexp = self.check_regexp.GetValue())
+        except re.error, e:
+            if show_error:
+                dialogs.error(self.editor,
+                    "Invalid regular expression:\n\n" + str(e).capitalize())
