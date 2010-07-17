b4f5be587842c8857b1e4c103194e5da6f594b4d
Luke McCarthy <luke@iogopro.co.uk>
2010-07-17 16:07:38 +0100

    Change to source dir.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "fsmonitor"))
+os.chdir(sys.path[0])
+sys.path.append(os.path.join(sys.path[0], "..", "fsmonitor"))
 
 import wx
 
