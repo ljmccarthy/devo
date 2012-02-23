1a009ab57dab5869702277e3d11d72cbb4d638aa
Luke McCarthy <luke@iogopro.co.uk>
2012-02-23 18:41:28 +0000

    Fixed: Don't count newlines, tabs, etc. as non-printable characters!

--- util.py
+++ util.py
@@ -25,10 +25,13 @@ if wx.Platform == "__WXGTK__":
 else:
     frozen_or_hidden_window = frozen_window
 
+non_printable_chars = "\x00\x01\x02\x03\x04\x05\x06\x07\x0E\x0F" + \
+    "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x7F"
+
 def count_non_printable(s):
     count = 0
     for c in s:
-        if ord(c) < 0x20:
+        if c in non_printable_chars:
             count += 1
     return count
 
