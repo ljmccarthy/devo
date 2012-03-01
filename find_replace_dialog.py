730cb461ef3d90864483d72c735283470efeeeee
Luke McCarthy <luke@iogopro.co.uk>
2012-03-01 22:04:45 +0000

    Populate combos before setting value.

--- find_replace_dialog.py
+++ find_replace_dialog.py
@@ -172,15 +172,15 @@ class FindReplaceDialog(wx.Dialog):
         self.Fit()
 
         if details is not None:
+            for item in details.find_history:
+                self.combo_find.Append(item)
+            for item in details.replace_history:
+                self.combo_replace.Append(item)
             self.combo_find.SetValue(details.find)
             self.combo_replace.SetValue(details.replace)
             self.check_case.SetValue(details.case)
             self.check_regexp.SetValue(details.regexp)
             self.check_reverse.SetValue(details.reverse)
-            for item in details.find_history:
-                self.combo_find.Append(item)
-            for item in details.replace_history:
-                self.combo_replace.Append(item)
 
         self.combo_find.SetFocus()
         self.combo_find.SetMark(0, len(self.combo_find.GetValue()))
