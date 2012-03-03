627ee188c8e299814b390526e67af7e486b49f5e
Luke McCarthy <luke@iogopro.co.uk>
2012-03-03 17:41:28 +0000

    Fixed running commands on Windows.

--- shell.py
+++ shell.py
@@ -1,22 +1,21 @@
 import sys, subprocess
 
-if sys.platform == "win32":
-    shell = "cmd"
-else:
-    shell = "bash"
-
 def run_shell_command(cmdline, pipe_output=True, **kwargs):
-    pipe = subprocess.PIPE if pipe_output else None
-    process = subprocess.Popen([shell],
-        stdin = subprocess.PIPE,
+    args = ["bash"] if sys.platform != "win32" else cmdline
+
+    process = subprocess.Popen(args,
+        stdin = subprocess.PIPE if sys.platform != "win32" else None,
         stdout = subprocess.PIPE if pipe_output else None,
         stderr = subprocess.STDOUT if pipe_output else None,
         bufsize = 1,
         close_fds = (sys.platform != "win32"),
-        shell = False,
+        shell = (sys.platform == "win32"),
         **kwargs)
-    process.stdin.write(cmdline)
-    process.stdin.close()
+
+    if sys.platform != "win32":
+        process.stdin.write(cmdline)
+        process.stdin.close()
+
     return process
 
 def kill_shell_process(process, force=False):
