df4d5daa5ec8f259da598d8fab072015adf7eff0
Luke McCarthy <luke@iogopro.co.uk>
2012-03-09 23:30:06 +0000

    Process command line arguments.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys, os
 os.chdir(sys.path[0])
 sys.path.append(os.path.join(sys.path[0], "..", "fsmonitor"))
 
+import getopt
 import wx
 
 class App(wx.App):
@@ -14,10 +15,8 @@ class App(wx.App):
     def OnInit(self):
         import async_wx
         async_wx.set_wx_scheduler()
-        from mainframe import MainFrame
         self.SetAppName("Devo")
-        self.mainframe = MainFrame()
-        self.SetTopWindow(self.mainframe)
+
         return True
 
     def MacOpenFile(self, filename):
@@ -25,10 +24,30 @@ class App(wx.App):
             self.mainframe.OpenEditor(filename)
         self.first_drop = False
 
+def process_args(args):
+    try:
+        opts, args = getopt.gnu_getopt(args, "", ["project="])
+    except getopt.GetoptError, e:
+        sys.stderr.write("devo: %s\n" % e)
+        sys.exit(1)
+
+    project = None
+    for opt, arg in opts:
+        if opt == "--project":
+            project = arg
+
+    from mainframe import MainFrame
+    mainframe = MainFrame(project)
+
+    for arg in args:
+        mainframe.OpenEditor(arg)
+
 def main():
     if wx.VERSION < (2,9):
         wx.InitAllImageHandlers()
     app = App()
+    mainframe = process_args(sys.argv[1:])
+    app.SetTopWindow(mainframe)
     app.MainLoop()
 
 if __name__ == "__main__":
