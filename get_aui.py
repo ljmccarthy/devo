61cfac5b7f7ddadfda7d0ed24bba302c86526833
Luke McCarthy <luke@iogopro.co.uk>
2012-03-11 20:24:07 +0000

    Moved get aui code in to a function.

--- get_aui.py
+++ get_aui.py
@@ -5,7 +5,11 @@ import os.path, subprocess
 def run(*args):
     subprocess.check_call(args)
 
-if os.path.isdir("aui"):
-    run("svn", "update", "aui")
-else:
-    run("svn", "checkout", "http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/aui", "aui")
+def get_aui():
+    if os.path.isdir("aui"):
+        run("svn", "update", "aui")
+    else:
+        run("svn", "checkout", "http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/aui", "aui")
+
+if __name__ == "__main__":
+    get_aui()
