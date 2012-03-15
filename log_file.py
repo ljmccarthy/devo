1d8284f35bd366a2f92337701e0208d1dc8e34b9
Luke McCarthy <luke@iogopro.co.uk>
2012-03-15 21:14:36 +0000

    Log errors to file.

---
+++ log_file.py
@@ -0,0 +1,48 @@
+import os, time
+import fileutil
+
+def get_log_contents(filename, max_log_size):
+    try:
+        with open(filename) as f:
+            f.seek(0, os.SEEK_END)
+            size = f.tell()
+            if size > max_log_size:
+                f.seek(-max_log_size, os.SEEK_END)
+                return "\n".join(f.read().split("\n")[1:])
+            else:
+                f.seek(0)
+                return f.read()
+    except Exception:
+        return ""
+
+def get_log_file(filename, max_log_size=1024**2):
+    return LogFileWrapper(filename, max_log_size)
+
+class LogFileWrapper(object):
+    def __init__(self, filename, max_log_size):
+        self.filename = filename
+        self.max_log_size = max_log_size
+        self.file = None
+
+    def write(self, s):
+        if self.file is None:
+            contents = get_log_contents(self.filename, self.max_log_size)
+            header_newline = ""
+            if contents:
+                header_newline = "\n" if contents[-1] == "\n" else "\n\n"
+            header = time.strftime(header_newline + "===== %Y-%m-%d %H:%M:%S =====\n")
+            file = open(self.filename, "w")
+            try:
+                file.write(contents)
+                file.write(header)
+                file.flush()
+            except Exception:
+                file.close()
+                raise
+            else:
+                self.file = file
+        self.file.write(s)
+
+    def close(self):
+        if self.file:
+            self.file.close()
--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python2
 
-import sys, os
+import sys, os, getopt, tempfile, traceback
 
 if not hasattr(sys, "frozen"):
     module_dir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(module_dir)
     sys.path.append(os.path.join(module_dir, "..", "fsmonitor"))
 
-import getopt
 import wx
 
 class App(wx.App):
     def __init__(self):
         wx.App.__init__(self, redirect=False)
+        self.SetAppName("Devo")
         self.first_drop = True
 
     def OnInit(self):
-        import async_wx
-        async_wx.set_wx_scheduler()
-        self.SetAppName("Devo")
-
-        return True
+        try:
+            import async_wx, log_file
+            async_wx.set_wx_scheduler()
+            log_filename = os.path.join(tempfile.gettempdir(), "devo-errors.log")
+            self.log_file = log_file.get_log_file(log_filename)
+            sys.stdout, self.stdout = self.log_file, sys.stdout
+            sys.stderr, self.stderr = self.log_file, sys.stderr
+            return True
+        except Exception:
+            message = "Devo failed to initialize due to the following error:\n\n" + traceback.format_exc()
+            dlg = wx.MessageDialog(None, message, "Devo Initialization Error", wx.OK | wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+            return False
 
     def MacOpenFile(self, filename):
         if not self.first_drop or hasattr(sys, "frozen"):
             self.mainframe.OpenEditor(filename)
         self.first_drop = False
+
+    def Shutdown(self):
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+        f, self.log_file = self.log_file, None
+        f.close()
 
 def process_args(args):
     try:
@@ -45,6 +60,8 @@ def process_args(args):
     for arg in args:
         mainframe.OpenEditor(arg)
 
+    return mainframe
+
 def main():
     if wx.VERSION < (2,9):
         wx.InitAllImageHandlers()
@@ -52,6 +69,7 @@ def main():
     mainframe = process_args(sys.argv[1:])
     app.SetTopWindow(mainframe)
     app.MainLoop()
+    app.Shutdown()
 
 if __name__ == "__main__":
     main()
