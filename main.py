da8411b78031b5a6e9a14ae50d56ecbe3b19c232
Luke McCarthy <luke@iogopro.co.uk>
2012-03-30 00:25:10 +0100

    Refactored app initialization.

--- main.py
+++ main.py
@@ -14,22 +14,48 @@ class App(wx.App):
         wx.App.__init__(self, redirect=False)
         self.SetAppName("Devo")
         self.first_drop = True
+        self.mainframe = None
 
     def OnInit(self):
+        return True
+
+    def Startup(self, args):
         try:
             import async_wx, log_file
             async_wx.set_wx_scheduler()
+
             if hasattr(sys, "frozen"):
                 log_filename = os.path.join(tempfile.gettempdir(), "devo-errors.log")
                 self.log_file = log_file.get_log_file(log_filename)
                 sys.stdout, self.stdout = self.log_file, sys.stdout
                 sys.stderr, self.stderr = self.log_file, sys.stderr
+
+            try:
+                opts, args = getopt.gnu_getopt(args, "", ["project="])
+            except getopt.GetoptError, e:
+                sys.stderr.write("devo: error: %s\n" % e)
+                return False
+
+            project = None
+            for opt, arg in opts:
+                if opt == "--project":
+                    project = arg
+
+            from mainframe import MainFrame
+            self.mainframe = MainFrame(project)
+            self.SetTopWindow(self.mainframe)
+
+            for arg in args:
+                self.mainframe.OpenEditor(arg)
+
             return True
+
         except Exception:
             message = "Devo failed to initialize due to the following error:\n\n" + traceback.format_exc()
             dlg = wx.MessageDialog(None, message, "Devo Initialization Error", wx.OK | wx.ICON_ERROR)
             dlg.ShowModal()
             dlg.Destroy()
+
             return False
 
     def MacOpenFile(self, filename):
@@ -44,37 +70,21 @@ class App(wx.App):
             f, self.log_file = self.log_file, None
             f.close()
 
-def process_args(args):
-    if sys.platform == "darwin" and hasattr(sys, "frozen"):
-        opts, args = [], []
-    else:
-        try:
-            opts, args = getopt.gnu_getopt(args, "", ["project="])
-        except getopt.GetoptError, e:
-            sys.stderr.write("devo: error: %s\n" % e)
-            sys.exit(1)
-
-    project = None
-    for opt, arg in opts:
-        if opt == "--project":
-            project = arg
-
-    from mainframe import MainFrame
-    mainframe = MainFrame(project)
-
-    for arg in args:
-        mainframe.OpenEditor(arg)
-
-    return mainframe
-
 def main():
     if wx.VERSION < (2,9):
         wx.InitAllImageHandlers()
+
+    if sys.platform == "darwin" and hasattr(sys, "frozen"):
+        args = []
+    else:
+        args = sys.argv[1:]
+
     app = App()
-    mainframe = process_args(sys.argv[1:])
-    app.SetTopWindow(mainframe)
-    app.MainLoop()
-    app.Shutdown()
+    if app.Startup(args):
+        app.MainLoop()
+        app.Shutdown()
+    else:
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
