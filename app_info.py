cf6ddfdac8a56194ad1297921a390be541d773cc
Luke McCarthy <luke@iogopro.co.uk>
2012-12-13 02:17:11 +0000

    Remove last digit of version number if it's 0.

--- app_info.py
+++ app_info.py
@@ -8,7 +8,7 @@ release_date = datetime.date(2012, 12, 13)
 
 version = (1, 0, 0)
 
-version_string = ".".join(str(x) for x in version)
+version_string = ".".join(str(x) for x in (version if version[2] != 0 else version[:2]))
 
 identifier = "com.iogopro.devo"
 
