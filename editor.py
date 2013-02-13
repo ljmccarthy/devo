596ea4d9e38e9f0e00304fbec39cc6a6c5fb3fc9
Luke McCarthy <luke@iogopro.co.uk>
2013-02-13 22:07:12 +0000

    Don't add newline to end of file.

--- editor.py
+++ editor.py
@@ -157,8 +157,6 @@ class Editor(StyledTextCtrl, wx.FileDropTarget):
     def SaveFile(self, path):
         text, self.file_encoding = encode_text(self.GetText(), self.file_encoding)
         text = clean_text(text)
-        if not text.endswith("\n"):
-            text += "\n"
         yield async_call(mkpath, os.path.dirname(path))
         yield async_call(atomic_write_file, path, text)
         self.modified_externally = False
