38f417db65ef990d12c7255ab47b0fd405bbe3a3
Luke McCarthy <luke@iogopro.co.uk>
2014-07-23 23:14:21 +0100

    Renamed get_aui.py to get_components.py, added code to check out fsmonitor from github.

---
+++ get_components.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2.7
+
+import os.path, subprocess
+
+def run(*args, **kwargs):
+    subprocess.check_call(args, **kwargs)
+
+def get_aui():
+    if os.path.isdir("aui"):
+        run("svn", "update", cwd="aui")
+    else:
+        run("svn", "checkout", "http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/aui", "aui")
+
+def get_fsmonitor():
+    if os.path.isdir("fsmonitor"):
+        run("git", "pull", cwd="fsmonitor")
+    else:
+        run("git", "clone", "https://github.com/shaurz/fsmonitor.git")
+
+
+def get_components():
+    get_aui()
+    get_fsmonitor()
+
+if __name__ == "__main__":
+    get_components()
