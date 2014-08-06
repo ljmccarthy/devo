e7d9b06092bd4904cd54c3c799bbe566eacb21a5
Luke McCarthy <luke@iogopro.co.uk>
2014-08-06 21:17:08 +0100

    Still need to ask git to get submodules.

--- get_components.py
+++ get_components.py
@@ -5,6 +5,9 @@ import os.path, subprocess
 def run(*args, **kwargs):
     subprocess.check_call(args, **kwargs)
 
+def get_submodules():
+    run("git", "submodule", "update", "--init")
+
 def get_aui():
     if os.path.isdir("aui"):
         run("svn", "update", cwd="aui")
@@ -12,6 +15,7 @@ def get_aui():
         run("svn", "checkout", "http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/aui", "aui")
 
 def get_components():
+    get_submodules()
     get_aui()
 
 if __name__ == "__main__":
