58e940a44652bd6b37051bfadf23c59b37b1dad1
Luke McCarthy <luke@iogopro.co.uk>
2014-08-06 21:10:23 +0100

    Added fsmonitor as a git submodule.

--- get_components.py
+++ get_components.py
@@ -11,16 +11,8 @@ def get_aui():
     else:
         run("svn", "checkout", "http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/aui", "aui")
 
-def get_fsmonitor():
-    if os.path.isdir("fsmonitor"):
-        run("git", "pull", cwd="fsmonitor")
-    else:
-        run("git", "clone", "https://github.com/shaurz/fsmonitor.git")
-
-
 def get_components():
     get_aui()
-    get_fsmonitor()
 
 if __name__ == "__main__":
     get_components()
